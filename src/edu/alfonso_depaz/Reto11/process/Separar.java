90ce848da123daa4b8e5ba7b5dcf448fbd0824a7
Reto 2 <5281niva@gmail.com>
2024-05-07 22:05:34 -0600

    Merge remote-tracking branch Evidencia_1/Evidencia_1' into Evidencia_3

--- src/edu/alfonso_depaz/Reto11/process/Separar.java
+++
@@ -1,8 +0,0 @@
-package edu.alfonso_depaz.Reto11.process;
-
-public class Separar {
-    public static String[] separar(String texto) {
-        // Se utiliza el método split() de la clase String para dividir el texto en palabras utilizando espacios en blanco como delimitador
-        return texto.split("\\s+"); // Separar por espacios en blanco
-    }
-}
--- src/edu/alfonso_depaz/Reto11/ui/Idioma.java
+++
@@ -1,9 +0,0 @@
-package edu.alfonso_depaz.Reto11.ui;
-
-public abstract class Idioma {
-    public abstract String Bienvenido();
-    public abstract String get_SeleccionarLibro();
-    public abstract String fin();
-    public abstract String get_opcionInvalida();
-    public abstract String get_PalabrasMasUsadas();
-}
