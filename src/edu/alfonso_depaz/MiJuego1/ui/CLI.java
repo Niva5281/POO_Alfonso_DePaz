7c86a8967a4669617c8e58fd73e9c6c8efdc1cba
Reto 2 <5281niva@gmail.com>
2024-05-13 09:53:18 -0600

    Merge remote-tracking branch Evidencia_1/Evidencia_1' into Evidencia_3

--- src/edu/alfonso_depaz/MiJuego1/ui/CLI.java
+++ src/edu/alfonso_depaz/MiJuego1/ui/CLI.java
@@ -11,10 +11,14 @@ public class CLI {
     /**
      * Juego de gato donde cumple con los parametros de la evidencia 3
      */
-    //En main se hacen todas las operaciones de entradas y salidas del usuario
+    //En main se manda a llamar el menu
     public static void main(String [] args){
         CLI.Menu();
     }
+
+    /**
+     * Se registra el idioma y luego se pregunta cual opcion de juego va a escoger y se va hacia el modo de juego
+     */
     public static void Menu(){
         //Se le pregunta al usuario el modo de juego a escojer y se va a ese apartado
         Scanner scanner=new Scanner(System.in);
@@ -31,7 +35,12 @@ public class CLI {
             System.out.println(lenguaje.get_Salida_Error_CaracterInvalido());
         }
     }
-    //Se define el lenguaje a utilizar "Español y ingles" utilizando un metodo
+
+    /**
+     * Se define el idioma utilizando un scanner
+     * @param scanner leer entradas del usuario
+     * @return el lenguaje escogido
+     */
     private static Idioma Seleccion_Idioma(Scanner scanner){
         Idioma lenguaje;
         System.out.println("1. Español");
@@ -53,6 +62,18 @@ public class CLI {
         }
         return lenguaje;
     }
+
+    /**
+     * Se agregan metodos para leer jugadores, utiliza un polimorfismo inecessario en la clase "process.metodos.jugadores"
+     * a partir de las clases, utiliza los metodos necesarios y acciones para ejecutar el programa
+     *
+     * objetivo: crear como checkpoints en donde el programa necesitara para el registro
+     *
+     * @param jg1 variable donde se almacena
+     * @param metodosJugadores metodos en donde realizan las accinoes necesarias
+     * @param lenguaje pasa el idioma seleccionado
+     * @return se regresa jg1
+     */
     public static String leerJg1(String jg1, Metodos_Jugadores metodosJugadores,Idioma lenguaje){
         jg1=metodosJugadores.Guardar_NombresJugadores_V1(jg1,lenguaje);
         return jg1;
@@ -69,12 +90,22 @@ public class CLI {
         cj2=metodosJugadores.Guardar_CaracteresJugadores_V1(cj2,lenguaje);
         return cj2;
     }
+
+    /**
+     * pregunta al usuario si desea jugar otra vez
+     * @param lenguaje
+     * @return el valor de la opcion si/no
+     */
     public int mostrarMenuReinicio(Idioma lenguaje) {
         Scanner scanner=new Scanner(System.in);
         System.out.println(lenguaje.get_Salida_JugarOtraVez());
         return scanner.nextInt();
     }
-    //Modo de juego de jugador contra jugador donde se utilizan todos los metodos requeridos para su modo de juego
+
+    /**
+     * Modo de juego de jugador contra jugador donde se utilizan todos los metodos requeridos para su modo de juego
+     * @param lenguaje Se pasa el lenguaje utilizado
+     */
     public static void Jugador_VS_Jugador(Idioma lenguaje){
         Scanner scanner=new Scanner(System.in);
         CLI cli=new CLI();
@@ -83,7 +114,11 @@ public class CLI {
         System.out.println("****************************************************");
         partida.jugar_JugadorVsJugador(scanner, cli,lenguaje);
     }
-    //Modo de juego de jugador contra CPU donde se utilizan todos los metodos requeridos para su modo de juego
+
+    /**
+     * Modo de juego de jugador contra CPU donde se utilizan todos los metodos requeridos para su modo de juego
+     * @param lenguaje
+     */
     public static void Jugador_VS_CPU(Idioma lenguaje){
         System.out.println(lenguaje.get_Salida_Bienvenida_ModoDeJuego_JgVSCPU());
         Scanner scanner=new Scanner(System.in);
